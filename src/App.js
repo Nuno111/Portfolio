e4455214e364618b0bc478e8b7530eef4dbb0014
nuno <nuno_c11@hotmail.com>
2021-06-30 01:07:26 +0100

    created structured

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 function App() {
-  return (
-    <div className="App">
-		<h1>Hello from React</h1>
-    </div>
-  );
+	return (
+		<div className="App font-bold text-xl">
+			<h1>Hello from React</h1>
+		</div>
+	);
 }
 
 export default App;
