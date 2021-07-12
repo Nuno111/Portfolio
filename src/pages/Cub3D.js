a84c05966da64e5f63ce8db5e6b863427387e62c
nuno <nuno_c11@hotmail.com>
2021-07-12 17:37:31 +0100

    working on projects

--- src/pages/Cub3D.js
+++ src/pages/Cub3D.js
@@ -1,6 +1,14 @@
 import ProjectTemplate from "../components/ProjectTemplate";
+import ProjectImg from "../assets/sampleproject.jpg";
 
 const Cub3D = () => {
+	const img = (
+		<img
+			src={ProjectImg}
+			alt="3d first person shooter game"
+			className="rounded-lg"
+		/>
+	);
 	return (
 		<ProjectTemplate
 			title="cub3D"
@@ -9,8 +17,28 @@ const Cub3D = () => {
 					necessitatibus laboriosam consequuntur
 					Facere possimus error ipsum maxime
 					magnam quaerat. Ex eaque repellendus
-					laborum ad eum Porro sed fugit debitis.
-"
+					laborum ad eum Porro sed fugit debitis."
+			objective="Lorem neque optio placeat blanditiis
+					animi? Expedita corrupti magni suscipit
+					necessitatibus laboriosam consequuntur
+					Facere possimus error ipsum maxime
+					magnam quaerat. Ex eaque repellendus
+					laborum ad eum Porro sed fugit debitis."
+			challenges="Lorem neque optio placeat blanditiis
+					animi? Expedita corrupti magni suscipit
+					necessitatibus laboriosam consequuntur
+					Facere possimus error ipsum maxime
+					magnam quaerat. Ex eaque repellendus
+					laborum ad eum Porro sed fugit debitis."
+			conclusion="Lorem neque optio placeat blanditiis
+					animi? Expedita corrupti magni suscipit
+					necessitatibus laboriosam consequuntur
+					Facere possimus error ipsum maxime
+					magnam quaerat. Ex eaque repellendus
+					laborum ad eum Porro sed fugit debitis."
+			img={img}
+			img1={img}
+			img2={img}
 		/>
 	);
 };
