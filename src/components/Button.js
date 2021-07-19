27203c5304b2278beeb1d28c733b78d30123e352
nuno <nuno_c11@hotmail.com>
2021-07-19 22:24:34 +0100

    made button smaller on mobile

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,17 +2,13 @@ const Button = ({ text }) => {
 	const dark =
 		"dark:hover:text-darkTxtPrimary dark:bg-darkBgPrimary dark:border-darkTxtPrimary dark:text-light";
 
-	const light =
-		"text-lightTxtPrimary border-lightTxtPrimary";
+	const light = "text-lightTxtPrimary border-lightTxtPrimary";
 
 	const general =
-		"m-4 py-4 px-8 whitespace-nowrap text-xl border-2 border-solid  font-bold rounded-full transform hover:scale-110 transition-transform duration-300 ease-linear";
+		"m-4 py-2 px-4 lg:py-4 lg:px-8 whitespace-nowrap text-xl border-2 border-solid  font-bold rounded-full transform hover:scale-110 transition-transform duration-300 ease-linear";
 
 	return (
-		<button
-			className={`${dark} ${light} ${general}`}
-			type="button"
-		>
+		<button className={`${dark} ${light} ${general}`} type="button">
 			{text}
 		</button>
 	);
