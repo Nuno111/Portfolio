20913fd185e7de8d462693d058232c8537d23231
nuno <nuno_c11@hotmail.com>
2021-07-20 11:17:05 +0100

    extracted app GA logic into a custom hook

---
+++ src/hooks/useGoogleAnalytics.js
@@ -0,0 +1,19 @@
+import { useLocation } from "react-router";
+import { useEffect } from "react";
+import ReactGA from "react-ga";
+
+const useGoogleAnalytics = () => {
+	let location = useLocation();
+
+	useEffect(() => {
+		if (!window.GA_INITIALIZED) {
+			ReactGA.initialize("UA-202596498-1");
+			window.GA_INITIALIZED = true;
+		}
+		ReactGA.set({ page: location.pathname });
+
+		ReactGA.pageview(window.location.pathname);
+	}, [location]);
+};
+
+export default useGoogleAnalytics;
