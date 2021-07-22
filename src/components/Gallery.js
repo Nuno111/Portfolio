05f2ae5b4846f9715f7eb7ca481b9c5828afb0b8
nuno <nuno_c11@hotmail.com>
2021-07-22 16:48:53 +0100

    about personal done

---
+++ src/components/Gallery.js
@@ -0,0 +1,51 @@
+import beachNightSriLanka from "../assets/beachNightSriLanka.jpg";
+import beachPhilippines from "../assets/beachPhilippines.jpg";
+import hatSriLanka from "../assets/hatSriLanka.jpg";
+import nineArchBridge from "../assets/nineArchBridge.jpg";
+import nineArchBridgeElla from "../assets/nineArchBridgeElla.jpg";
+import sriPadaSriLanka from "../assets/sriPadaSriLanka.jpg";
+import sunsetPhilippines from "../assets/sunsetPhilippines.jpg";
+import isleOfWight from "../assets/isleOfWight.jpg";
+import jardimBotanicoCoimbra from "../assets/jardimBotanicoCoimbra.jpg";
+
+const Gallery = () => {
+  const imgClass = "w-full lg:w-1/4";
+  return (
+    <section className="flex flex-wrap gap-4 justify-center">
+      <img
+        src={beachNightSriLanka}
+        className={imgClass}
+        alt=""
+      />
+      <img className={imgClass} src={hatSriLanka} alt="" />
+      <img
+        className={imgClass}
+        src={nineArchBridge}
+        alt=""
+      />
+      <img
+        className={imgClass}
+        src={nineArchBridgeElla}
+        alt=""
+      />
+      <img
+        className={imgClass}
+        src={sriPadaSriLanka}
+        alt="hh"
+      />
+      <img
+        className={imgClass}
+        src={sunsetPhilippines}
+        alt=""
+      />
+      <img
+        className={imgClass}
+        src={jardimBotanicoCoimbra}
+        alt=""
+      />
+      <img className={imgClass} src={isleOfWight} alt="" />
+    </section>
+  );
+};
+
+export default Gallery;
