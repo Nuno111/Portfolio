16e023412d3f58777e0c436ddfc5681f1e886692
nuno <nuno_c11@hotmail.com>
2021-07-29 09:27:37 +0100

    promovedAboutAccessibility

--- src/components/HomeAboutCard.js
+++ src/components/HomeAboutCard.js
@@ -38,24 +38,27 @@ const HomeAboutCard = ({
       <ArrowrightSvg className="inline w-10 h-10 fill-current group-hover:animate-spin" />
     </Link>
   );
+
   return (
-    <li
-      className={`grid gap-10 grid-cols-1 items-center justify-items-center
+    <li>
+      <article
+        className={`grid gap-10 grid-cols-1 items-center justify-items-center
 		py-6 lg:grid-cols-4`}
-    >
-      {svg}
-      <div
-        className={`flex flex-col gap-6 items-center justify-center text-center
-        lg:col-end-4 lg:col-start-2 lg:items-start lg:justify-start lg:text-left`}
       >
-        <h2 className="text-2xl font-bold xl:text-3xl">
-          {title}
-        </h2>
-        <p className="leading-loose lg:text-2xl">
-          {content1}
-        </p>
-        {link}
-      </div>
+        {svg}
+        <div
+          className={`flex flex-col gap-6 items-center justify-center text-center
+        lg:col-end-4 lg:col-start-2 lg:items-start lg:justify-start lg:text-left`}
+        >
+          <h2 className="text-2xl font-bold xl:text-3xl">
+            {title}
+          </h2>
+          <p className="leading-loose lg:text-2xl">
+            {content1}
+          </p>
+          {link}
+        </div>
+      </article>
     </li>
   );
 };
